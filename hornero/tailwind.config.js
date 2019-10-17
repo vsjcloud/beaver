30f5168a422f132f0df406559950bf2cf4b8fe2c
Thuy Luc Tran <erathorus@gmail.com>
2019-10-17 21:20:39 +1100

    Add colors and shadows

--- hornero/tailwind.config.js
+++ hornero/tailwind.config.js
@@ -1,24 +1,72 @@
 module.exports = {
   theme: {
     fontFamily: {
-      body: ['"Lato"', "sans-serif"]
+      body: ['"Lato"', "sans-serif"],
     },
     colors: {
+      primary: {
+        1: "var(--primary-1)",
+        2: "var(--primary-2)",
+        3: "var(--primary-3)",
+        4: "var(--primary-4)",
+        5: "var(--primary-5)",
+      },
+      success: {
+        1: "var(--success-1)",
+        2: "var(--success-2)",
+        3: "var(--success-3)",
+        4: "var(--success-4)",
+        5: "var(--success-5)",
+      },
+      warning: {
+        1: "var(--warning-1)",
+        2: "var(--warning-2)",
+        3: "var(--warning-3)",
+        4: "var(--warning-4)",
+        5: "var(--warning-5)",
+      },
+      danger: {
+        1: "var(--danger-1)",
+        2: "var(--danger-2)",
+        3: "var(--danger-3)",
+        4: "var(--danger-4)",
+        5: "var(--danger-5)",
+      },
       "dark-gray": {
         1: "var(--dark-gray-1)",
         2: "var(--dark-gray-2)",
         3: "var(--dark-gray-3)",
         4: "var(--dark-gray-4)",
-        5: "var(--dark-gray-5)"
+        5: "var(--dark-gray-5)",
       },
       gray: {
-
-      }
+        1: "var(--gray-1)",
+        2: "var(--gray-2)",
+        3: "var(--gray-3)",
+        4: "var(--gray-4)",
+        5: "var(--gray-5)",
+      },
+      "light-gray": {
+        1: "var(--light-gray-1)",
+        2: "var(--light-gray-2)",
+        3: "var(--light-gray-3)",
+        4: "var(--light-gray-4)",
+        5: "var(--light-gray-5)",
+      },
     },
     boxShadow: {
-      "elevation-0": "var(--shadow-elevation-0)"
-    }
+      "elevation-0": "var(--shadow-elevation-0)",
+      "elevation-1": "var(--shadow-elevation-1)",
+      "elevation-2": "var(--shadow-elevation-2)",
+      "elevation-3": "var(--shadow-elevation-3)",
+      "elevation-4": "var(--shadow-elevation-4)",
+      "dark-elevation-0": "var(--shadow-dark-elevation-0)",
+      "dark-elevation-1": "var(--shadow-dark-elevation-1)",
+      "dark-elevation-2": "var(--shadow-dark-elevation-2)",
+      "dark-elevation-3": "var(--shadow-dark-elevation-3)",
+      "dark-elevation-4": "var(--shadow-dark-elevation-4)",
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 };

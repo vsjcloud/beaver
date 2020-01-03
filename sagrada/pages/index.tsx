83e73ece68b6ef515476dfdf7a7b343a134ca44e
Thuy Luc Tran <erathorus@gmail.com>
2020-01-03 13:27:33 +0700

    Initialize main site

---
+++ sagrada/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import {BaseLayout} from "../components/layout/BaseLayout";
+
+export default function Home(): React.ReactElement {
+  return (
+    <BaseLayout>
+      <div>Hello</div>
+    </BaseLayout>
+  );
+}
